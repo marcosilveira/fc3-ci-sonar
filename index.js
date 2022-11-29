e8baa15ecb7c85a6f485fddd8bef20627eb19737
Marco Silveira <marcosilveira39@gmail.com>
2022-11-29 10:40:57 -0300

    fix: add coverage test

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function soma(a, b) {
     return a + b;
 }
 
-console.log(soma(1,2));
+console.log(soma(10,20));
 
 module.exports = { soma }
--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 //import soma from ".";
 
 const index = require("./index");
-test('adds 1 + 2 to equal 3', () => {
-    const result = index.soma(1,2);
-    expect(result).toBe(3);
+test('adds 10 + 20 to equal 30', () => {
+    const result = index.soma(10,20);
+    expect(result).toBe(30);
 });
